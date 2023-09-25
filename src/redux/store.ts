741b0c15a2cc91f0a0757177569befa9a9249bb8
Kamrul Saad <skamrul2680@gmail.com>
2023-09-25 15:35:11 +0600

    Added HOC for providers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from "./rootReducer";
 
 export const store = configureStore({
-  reducer: {
-  },
-})
+  reducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
